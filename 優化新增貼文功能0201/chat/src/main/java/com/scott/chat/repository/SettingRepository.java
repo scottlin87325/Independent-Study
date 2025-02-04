7ee291a5ca0d57606b2005f7098192e8c3005377
samlin9081 <a951711221001@yahoo.com.tw>
2025-02-04 13:06:23 +0800

    16516

--- 優化新增貼文功能0201/chat/src/main/java/com/scott/chat/repository/SettingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scott.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.scott.chat.model.Member;
-
-public interface SettingRepository extends JpaRepository<Member, Integer> {
-    Member findByEmail(String email);
-}

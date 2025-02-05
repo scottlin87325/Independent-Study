58868371fe614749604552e69b8dc23c1efe5995
w5030625 <w5030622@gmail.com>
2025-02-05 15:44:17 +0800

    Add files via upload

--- src/main/java/com/scott/chat/config/WebSocketConfig.java
+++ src/main/java/com/scott/chat/config/WebSocketConfig.java
@@ -16,8 +16,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:*")
-                .withSockJS();
+        registry.addEndpoint("/ws").withSockJS();
     }
 }

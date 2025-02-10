65a994ae2a94281de0dd5ec938544ec09d1b85f0
w5030625 <w5030622@gmail.com>
2025-02-10 14:31:41 +0800

    Update MemberService.java

--- chat/src/main/java/com/scott/chat/service/MemberService.java
+++ chat/src/main/java/com/scott/chat/service/MemberService.java
@@ -14,12 +14,33 @@ public class MemberService {
 	private MemberRepository memberRepository;
 		
 	private final BCryptPasswordEncoder passwordEncoder= new BCryptPasswordEncoder();
+	 // 讀取默認頭像
+    private byte[] getDefaultPhoto() {
+        try {
+            // 從資源文件夾讀取默認圖片
+            ClassPathResource resource = new ClassPathResource("static/images/default-avatar.png");
+            return StreamUtils.copyToByteArray(resource.getInputStream());
+        } catch (IOException e) {
+            // 如果讀取失敗，返回一個空的 byte 數組
+            return new byte[0];
+        }
+    }
+	
+	
 	//新增會員
 	public void addMember(Member member) {
 		member.setPassword(passwordEncoder.encode(member.getPassword()));
-		if (member.getPostcount() == null) {
-	        member.setPostcount(0); // 如果 Post_count 是 null，則設為 0
-	    }
+		member.setPostcount(0); // 如果 Post_count 是 null，則設為 0
+		member.setTelephone("090000000");
+	    member.setBirthday("2000-01-01");
+	    member.setGender("男");
+		
+	    
+	 // 設定默認頭像
+        if (member.getMemberphoto() == null) {
+            member.setMemberphoto(getDefaultPhoto());
+        }
+		
 		memberRepository.save(member);
 	}
 	
